788946e453c123656f3e9d2d2684617d00c03326
Vlad <vl.zayko@gmail.com>
2019-06-15 23:24:02 +0300

    Add more vocablurary

--- src/Makemyday.php
+++ src/Makemyday.php
@@ -1,15 +1,19 @@
 <?php
-declare(strict_types=1);
+declare(strict_types = 1);
 
 namespace baddoctor\makemyday;
 
-
+/**
+ * Class Makemyday
+ */
 class Makemyday implements MakemydayInterface
 {
-    private $days = ['good', 'bad'];
+    private $vocablurary = ['very good', 'superb', 'outstanding', 'exceptional', 'marvelous', 'wonderful', 'magnificent', 'preeminent', 'perfect', 'matchless', 'unbeatable', 'peerless', 'supreme', 'prime', 'first-rate', 'first-class', 'superlative', 'splendid', 'fine', 'beautiful', 'exemplary', 'A1', 'ace', 'great', 'terrific', 'tremendous', 'fantastic', 'fabulous', 'splendiferous', 'fab', 'top-notch', 'dandy', 'divine', 'blue-ribbon', 'blue-chip', 'bang-up', 'skookum', 'class', 'awesome', 'magic', 'wicked', 'mean', 'cool', 'out', 'of this world', 'hunky-dory', 'A-OK', 'brilliant', 'killer'];
 
     public function go(): string
     {
-        return $this->days[random_int(0, count($this->days) - 1)];
+        $day = $this->vocablurary[random_int(0, count($this->vocablurary) - 1)];
+
+        return sprintf('Today is %s day!', $day);
     }
 }
--- src/MakemydayInterface.php
+++ src/MakemydayInterface.php
@@ -3,7 +3,9 @@ declare(strict_types=1);
 
 namespace baddoctor\makemyday;
 
-
+/**
+ * Interface MakemydayInterface
+ */
 interface MakemydayInterface
 {
     public function go() : string;
